1a3cd0548a3492224e03adfcd55f5f463a9a687e
Tokarev Dmitry <modelist61@bk.ru>
2020-12-24 11:37:25 +0300

    Initial Commit

---
+++ HW3.5 contactList/HW3_5_contactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_5_contactListApp.swift
+//  HW3.5 contactList
+//
+//  Created by Dmitry Tokarev on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HW3_5_contactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
